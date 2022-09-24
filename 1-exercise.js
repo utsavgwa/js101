b815e859cbf3479ed6ebd78fa90c7089ff93a1a6
Utsav <utsavgwa@gmail.com>
2022-09-24 13:15:03 +0530

    first exerise to understand js console

---
+++ 1-exercise.js
@@ -0,0 +1,16 @@
+// var greeting = "something";
+// var something = greeting/2;
+
+// something;
+// console.log(typeof something);
+
+// console.log(Number.isNaN(something));
+// console.log(Number.isNaN(greeting));
+
+// console.log(typeof greeting);
+
+var yesterday = new Date("2019-01-01");
+console.log(yesterday.toUTCString());
+
+anotherDAy = new Date("September 8, 2022");
+console.log(anotherDAy.toUTCString());
