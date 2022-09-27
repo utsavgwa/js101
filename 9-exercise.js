4632fb101a47169152f080cf532d32e3d323a656
Utsav <utsavgwa@gmail.com>
2022-09-27 15:19:19 +0530

    exercise 9 prog. primer

--- 9-exercise.js
+++ 9-exercise.js
@@ -1,5 +1,25 @@
-
-function addTwo(a,b) {
-    return a + b;
+// TODO: define addFavoriteBook(..) function
+function addFavoriteBook(bookName) {
+	if (!bookName.includes("Great")) {
+		favoriteBooks.push(bookName);
+	}
 }
-addTwo(5,"10")
+
+// TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+	console.log(`Favorite Books: ${favoriteBooks.length}`);
+	for (let bookName of favoriteBooks) {
+		console.log(bookName);
+	}
+}
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
+printFavoriteBooks();
