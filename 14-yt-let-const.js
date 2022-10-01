8f109665c32df688fcfd5ca2581d1d58656469c4
Utsav <utsavgwa@gmail.com>
2022-10-01 22:40:19 +0530

    diff between var, let and const

---
+++ 14-yt-let-const.js
@@ -0,0 +1,11 @@
+//gets stored in different memory location
+let a =10;
+console.log(a);
+
+//gets stored in different memory location, as let
+const c=30;
+console.log(c);
+
+// this gets attached to the window object
+var b = 20;
+console.log(b);
