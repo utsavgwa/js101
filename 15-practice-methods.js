e91f4bb5e77223a662cdedabf0340b81b174b549
Utsav <utsavgwa@gmail.com>
2022-10-04 23:10:08 +0530

    practice .include and line break

---
+++ 15-practice-methods.js
@@ -0,0 +1,12 @@
+// .includes() method in JS
+var students = [1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10];
+console.log(students);
+
+if (students.includes(5)) {
+    console.log("Student 5 is present");
+} else {
+    console.log("Student 5 is not present");
+};
+
+//creating a line break in JS
+console.log("\n");
